6ffea90784c3c1f6a618769f98561f39550ac70d
sultan-addweb <sultan@addwebsolution.in>
2023-12-15 18:06:44 +0530

    -- responsive_text_widget_test.dart

--- test/responsive_text_widget_test.dart
+++ test/responsive_text_widget_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:responsive_text_widget/responsive_text_widget.dart';
-
 void main() {
   test('adds one to input values', () {
     // final calculator = Calculator();
